9223ce2f65e1a2384b100598e0c3dddc7e518f2a
FR_naoki <naoki.maekawa.511@gmail.com>
2024-02-24 16:53:08 +0900

    リファクトリング②-返信メッセージ作成処理

--- app/Http/Controllers/Api/V1/LineController.php
+++ app/Http/Controllers/Api/V1/LineController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Services\Deliverer;
+use App\Services\ReplyMessageGenerator;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use LINE\LINEBot;
@@ -48,25 +49,10 @@ class LineController extends Controller
             $messageText = $event->message->text;
             Log::debug($replyToken);
 
-            switch ($messageText) {
-                case '今日の天気は？':
-                    // 天気APIを使って情報を取得してきたら正しい情報にできる
-                    $replyMessage = 'は、晴れかな・・・（しらんけど）';
-                    break;
-                case '元気？':
-                    $replyMessage = 'はい、元気です。あなたは？';
-                    break;
-                case '後ウマイヤ朝の最盛期王は？':
-                    $replyMessage = 'アブド＝アッラフマーン３世';
-                    break;
-                default:
-                    if (strpos($messageText, '？') !== false) {
-                        // 疑問符が含まれている場合(部分一致)
-                        $replyMessage = '「今日の天気は？」という質問に答える事ができますよ！';
-                    } else {
-                        $replyMessage = 'すみません、よくわかりません';
-                    }
-            }
+            //　2. 受け取ったメッセージの内容から返信するメッセージを作成
+            $generator = new ReplyMessageGenerator();
+            $replyMessage = $generator->generate($messageText);
+
             // 3. 返信メッセージを返信先に送信
 
             $deliverer = new Deliverer(env('LINE_CHANNEL_ACCESS_TOKEN', env('LINE_CHANNEL_SECRET')));
